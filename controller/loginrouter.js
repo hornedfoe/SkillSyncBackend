e543bac20be1614808f38551634458c14c328e1d
Hornedfoe <hornedfoe@gmail.com>
2024-04-01 19:18:56 +0530

    backend

--- controller/loginrouter.js
+++ controller/loginrouter.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 let router = express.Router();
 
-const { login, register } = require('../service/loginservice')
+const { login, register , sendOtp , validateOtp } = require('../service/loginservice')
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/sendOtp' , sendOtp);
+router.post('/validateOtp' , validateOtp);
 
 module.exports = router;
